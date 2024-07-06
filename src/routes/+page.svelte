37f226aa76abb36041a92d1a444509ac11c88f34
Tyler Wojciechowski <106572294+JustSomeGuyy@users.noreply.github.com>
2024-07-06 15:22:55 -0500

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Converison from "../components/Converison.svelte";
+import FaQs from "../components/FAQs.svelte";
+import Hero from "../components/Hero.svelte";
+	import Product from "../components/Product.svelte";
+	import Reviews from "../components/Reviews.svelte";
+
+</script>
+<main class="flex flex-col">
+    <Hero />
+    <Product />
+    <Reviews />
+    <FaQs />
+    <Converison />
+</main>
